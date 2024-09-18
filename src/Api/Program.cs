60e6972778e266bb738471f938b52939c9279ecc
Alan Bruhns <alanbruhns@gmail.com>
2024-09-17 22:54:42 -0300

    Inttial commit

---
+++ src/Api/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    WebRootPath = "wwwroot",
+    ApplicationName = typeof(Program).Assembly.FullName,
+    EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production",
+    ContentRootPath = Directory.GetCurrentDirectory(),
+});
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80); // HTTP
+});
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks(); // Add health check services
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHealthChecks("/healthz"); // Liveness probe endpoint
+app.UseHealthChecks("/readyz");  // Readiness probe endpoint
+
+app.MapGet("/hello", () =>
+{
+    return Ok("Hello");
+})
+.WithName("GetHello")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
